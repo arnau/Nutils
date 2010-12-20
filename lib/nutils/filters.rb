3ee16069e6c25559c1a8982a816e6f867092b0df
Arnau Siches <asiches@gmail.com>
2010-12-19 21:51:21 -0500

    Fix Sprockets filter

--- lib/nutils/filters.rb
+++ lib/nutils/filters.rb
@@ -1,12 +1,12 @@
 module Nutils
   module Filters
     autoload "Beautify", "nutils/filters/beautify"
-    autoload "Sprockets", "nutils/filters/sprockets"
+    autoload "SprocketWheel", "nutils/filters/sprockets"
     autoload "SvgToPng", "nutils/filters/svg2png"
     autoload "YuiCompressor", "nutils/filters/yuicompressor"
 
     ::Nanoc3::Filter.register "::Nutils::Filters::Beautify", :beautify
-    ::Nanoc3::Filter.register "::Nutils::Filters::Sprockets", :sprockets
+    ::Nanoc3::Filter.register "::Nutils::Filters::SprocketWheel", :sprockets
     ::Nanoc3::Filter.register "::Nutils::Filters::SvgToPng", :svg_to_png
     ::Nanoc3::Filter.register "::Nutils::Filters::YuiCompressor", :yuicompressor
   end
