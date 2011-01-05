417b05805ac915208a6f9625ccf5847f533cf4ca
Arnau Siches <asiches@gmail.com>
2011-01-04 22:13:23 -0500

    Add crop filter

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ A set of utilities for Nanoc3.
 ## Filters
 
 * **beautify** Retabs HTML code. Tested in Mac OS X 10.6, Ruby 1.8.7 and Nanoc 3.1.6.
+* **crop** Extracts the specified rectangle from the image.. Tested in Mac OS X 10.6, Ruby 1.8.7 and Nanoc 3.1.6.
 * **sprockets** Runs the content through [Sprockets](http://getsprockets.org)
 * **svg_to_png** Converts an SVG to PNG. Tested in Mac OS X 10.6, Ruby 1.8.7, Nanoc 3.1.6 and Batik 1.7.
 * **yuicompressor** Compress the content with [YUI Compressor](http://developer.yahoo.com/yui/compressor/). Tested in Mac OS X 10.6, Ruby 1.8.7, Nanoc 3.1.6 and yui-compressor 0.9.1.
---
+++ lib/nutils/filters/crop.rb
@@ -0,0 +1,52 @@
+module Nutils
+  module Filters
+
+    # @author Arnau Siches
+    #
+    # @version 0.1.0
+    #
+    # @note Requires «RMagick»
+    class RMagick < Nanoc3::Filter
+      
+      identifier :crop
+      type :binary
+
+
+      # Runs the content through {http://www.simplesystems.org/RMagick/doc/image1.html#crop RMagick#crop}.
+      #
+      # @param [String] filename The filename of the temporal file to filter.
+      #
+      # @option params [Integer] :x The x-offset of the rectangle relative 
+      #   to the upper-left corner of the image.
+      # 
+      # @option params [Integer] :y The y-offset of the rectangle relative 
+      #   to the upper-left corner of the image.
+      # 
+      # @option params [String] :width The width of the rectangle. This param can
+      #   handle string-expressions like +"image.columns - 1"+ where +image.columns+
+      #   is the width of the input image.
+      # 
+      # @option params [String] :height The height of the rectangle. This param can
+      #   handle string-expressions like +"image.rows - 1"+ where +image.rows+
+      #   is the height of the input image.
+      # 
+      # @return [File] The filtered content.
+      def run(filename, opts = {})
+        require 'RMagick'
+
+        raise ArgumentError, "x, y, width and height are required parameters." unless opts[:x] and opts[:y] and opts[:width] and opts[:height]
+
+        image = Magick::ImageList.new filename
+
+        width = eval(opts[:width])
+        height = eval(opts[:height])
+
+        image.crop(opts[:x], opts[:y], width, height).write output_filename
+
+      rescue Exception => e
+        raise ArgumentError, "#{e}\nSome parameters cannot be promoted to Integer."
+      end
+
+    end
+  end
+end
