9215d5b2f192d8228c61c549f70e0d7b9742ed58
Arnau Siches <asiches@gmail.com>
2011-08-17 20:07:17 -0400

    Fix beautify test

--- test/filters/test_beautify.rb
+++ test/filters/test_beautify.rb
@@ -12,7 +12,7 @@ class Nutils::Filters::BeautifyTest < MiniTest::Unit::TestCase
       filter = Nutils::Filters::Beautify.new
 
       # Set content
-      raw_content = %(
+      raw_content = %q(
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
 <head>
